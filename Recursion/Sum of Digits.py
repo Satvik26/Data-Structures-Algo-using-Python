e286bbe7dd4fd6e86e881bfbea5d2aa7ef0c42b6
Satvik Bisht <satvikbisht2699@gmail.com>
2022-05-23 11:48:43 +0530

    Create Sum of Digits.py

---
+++ Recursion/Sum of Digits.py
@@ -0,0 +1,33 @@
+"""
+Sum of digits (recursive)
+Send Feedback
+Write a recursive function that returns the sum of the digits of a given integer.
+Input format :
+Integer N
+Output format :
+Sum of digits of N
+Constraints :
+0 <= N <= 10^9
+Sample Input 1 :
+12345
+Sample Output 1 :
+15
+Sample Input 2 :
+9
+Sample Output 2 :
+9
+"""
+# Code :
+
+def sumOfDigit(n):
+    
+    if n == 0:
+        return 0
+    so = sumOfDigit(int(n/10))
+    return   n%10 + so
+
+
+
+
+n = int(input())
+print(sumOfDigit(n))
